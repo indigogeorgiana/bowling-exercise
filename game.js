8aebb13a3edcea723b0334118c9ccf9d8de447a6
Matthew McGregor <Mattjackmcgregor@gmail.com>
2018-05-30 15:42:20 +1200

    linked functions to scoregame

--- game.js
+++ game.js
@@ -1,41 +1,37 @@
- Score 119:
+// Score 119:
 
- var frames = [
+var frames = [
   [1, 2], [6, 4], [5, 4], [10, 0], [7, 2], [10, 0], [10, 0], [5, 2], [7, 0], [4, 4]
- ]
+]
+scoreGame(frames)
 
-function bowling() {
-  total = ""
+function scoreGame (frames) {
   for (i = 0; i < frames.length; i++) {
-    if (index[0] == 10) {
-      strike()
-    } else if (index[0] + index[1] == 10) {
-     spare()
-    } else if (index[0] + index[1] < 10){
-    open()
+    if (frames[i][0] == 10) {
+      scoreStrikeFrame()
+    } else if (frames[i][0] + frames[i][1] == 10) {
+      scoreSpareFrame()
+    } else {
+      scoreOpenFrame(frames[i])
     }
   }
 }
 
+function scoreOpenFrame (frame) {
+  return frame[0] + frame[1]
+}
 
- 
+function scoreSpareFrame (frame) {
+return 10 + 
+}
 
-function open (a, b) {
-  frames[i]
-  total[index[0]+ index[1]]
- }
+function socreStrikeFrame (frame) {
 
- function spare () {
+}
 
- }
+function gutterball () {
 
- function strike () {
-
- }
-
- function gutterball () {
-
- }
+}
 
 // Score 141:
 //
