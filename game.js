b515ef9f784750da0b90fd2f6d890f643db5a26e
Sungjun Park <sjpark731015@gmail.com>
2018-05-30 16:39:06 +1200

    not yet

--- game.js
+++ game.js
@@ -6,6 +6,33 @@
 //
 // Score 141:
 //
-// var frames = [
-//   [1, 2], [6, 4], [5, 4], [10, 0], [7, 2], [10, 0], [10, 0], [5, 2], [7, 0], [10, 10, 10]
-// ]
+
+var frames = [
+  [1, 2], [6, 4], [5, 4], [10, 0], [7, 2], [10, 0], [10, 0], [5, 2], [7, 0], [10, 10, 10]
+]
+
+function bowling (framse) {
+  let first = 0
+  let second = 0
+  let spare = 0
+  let strike = 0
+  let total = 0
+  for (let i = 0; i < frames.length; i++) {
+    alert(tmpCal(frames[0]))
+  }
+}
+
+function tmpCal (arr) {
+  let first = arr[0]
+  let second = arr[1]
+  let sum = first + second
+
+  if (first === 10 || second === 10) {
+    return 'strike'
+  }
+  if (sum === 10) {
+    return 'spare'
+  } else if (sum < 10) {
+    return 'open'
+  }
+}
