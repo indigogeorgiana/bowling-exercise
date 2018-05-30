c0029a19d3c77d7908db1e8c3b2e7d1a3a6537e5
ZeBin Xiao <juniorxiao01@gmail.com>
2018-05-30 18:36:34 +1200

    third bowling update

--- test/open.test.js
+++ test/open.test.js
@@ -3,7 +3,7 @@ const open = require('../game.js').open
 test('open adds 2 numbers within in a frame', function () {
   // Arrange
   const expected = 9
-  const bowlingFrames = require('../game.js').bowlingFrames
+  // const bowlingFrames = require('../game.js').bowlingFrames
 
   // Act
   const actual = open([2, 7])
