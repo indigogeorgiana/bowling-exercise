91622f4439bc57bfbbef8ad9104ddb30bd1d661c
Matthew McGregor <Mattjackmcgregor@gmail.com>
2018-05-30 15:08:29 +1200

    basic temp

--- game.js
+++ game.js
@@ -1,9 +1,42 @@
-// Score 119:
-//
-// var frames = [
-//   [1, 2], [6, 4], [5, 4], [10, 0], [7, 2], [10, 0], [10, 0], [5, 2], [7, 0], [4, 4]
-// ]
-//
+ Score 119:
+
+ var frames = [
+  [1, 2], [6, 4], [5, 4], [10, 0], [7, 2], [10, 0], [10, 0], [5, 2], [7, 0], [4, 4]
+ ]
+
+function bowling() {
+  total = ""
+  for (i = 0; i < frames.length; i++) {
+    if (index[0] == 10) {
+      strike()
+    } else if (index[0] + index[1] == 10) {
+     spare()
+    } else if (index[0] + index[1] < 10){
+    open()
+    }
+  }
+}
+
+
+ 
+
+function open (a, b) {
+  frames[i]
+  total[index[0]+ index[1]]
+ }
+
+ function spare () {
+
+ }
+
+ function strike () {
+
+ }
+
+ function gutterball () {
+
+ }
+
 // Score 141:
 //
 // var frames = [
