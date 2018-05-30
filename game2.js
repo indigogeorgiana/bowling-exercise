ab3bac7d8ec321642f8003cee698c072b7c46421
sam-by-name <sam-sanders@hotmail.co.uk>
2018-05-30 21:37:51 +1200

    because

--- game2.js
+++ game2.js
@@ -1,19 +1,21 @@
 module.exports = {
-    bowling
+  round1
 }
 
 let first = 0
 let second = 0
+let third = 0
 let spare = 0
 let strike = 0
 let total = 0
 
+// , [10, 6, 10]
 
 const frames = [
-  [3, 6], [10,0], [0, 9], [0,0], [1, 6], [7, 2], [3, 3], [8, 1], [0, 6], [10, 6, 10]
+  [3, 6], [10, 0], [0, 9], [0, 0], [1, 6], [7, 2], [3, 3], [8, 1], [0, 6]
 ]
 
-function round1() {
+function round1 () {
   for (let i = 0; i < frames.length; i++) {
     first = frames[i][0]
     second = frames[i][1]
@@ -21,9 +23,9 @@ function round1() {
       finalRound()
     } else if (strike === 20) {
       total += (strike + first)
-    } else if (strike = 10 && first !== 10) {
-      (total = (strike + first + second))
-    } else if (strike = 10 && first === 10) {
+    } else if (strike === 10 && first !== 10) {
+      (total += (strike + first + second))
+    } else if (strike === 10 && first === 10) {
       strike = 20
     } else if (spare === 10) {
       (total += spare)
@@ -31,33 +33,29 @@ function round1() {
       strike += 10
     } else if (first + second === 10) {
       spare += 10
-    } else { 
-      total += (first + second)        
+    } else {
+      total += (first + second)
     }
   }
 }
 
-function finalRound() {
-  for (let i = 0; i < frames.length; i++) {
-    first = frames[i][0]
-    second = frames[i][1]
-    third = frames[i][2]
-    if (strike === 20) {
-      total += (strike + first + second + third)
-    } else if (strike = 10 && first !== 10) {
-      (total = (strike + first + second + third))
-    } else if (strike = 10 && first === 10) {
-      strike = 20
-    } else if (spare === 10) {
-      (total += spare)
-    } else if (first === 10) {
-      strike += 10
-    } else if (first + second === 10) {
-      spare += 10
-    } else { 
-      total += (first + second )        
-    }
+function finalRound () {
+  first = frames[9][0]
+  second = frames[9][1]
+  third = frames[9][2]
+  if (strike === 20) {
+    total += (strike + first + second + third)
+  } else if (strike === 10 && first !== 10) {
+    (total += (strike + first + second + third))
+  } else if (strike === 10 && first === 10) {
+    strike = 20
+  } else if (spare === 10) {
+    (total += spare)
+  } else if (first === 10) {
+    strike += 10
+  } else if (first + second === 10) {
+    spare += 10
+  } else {
+    total += (first + second)
   }
-}
-
 }
